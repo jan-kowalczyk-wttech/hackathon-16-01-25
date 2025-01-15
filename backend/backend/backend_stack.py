ed952abf1fbab496bda4a6e97d15a47621bfa0e1
jan.kowalczyk <jan.kowalczyk@vml.com>
2025-01-15 17:52:30 +0100

    shared dependencies layer

--- backend/backend/backend_stack.py
+++ backend/backend/backend_stack.py
@@ -2,13 +2,26 @@ from aws_cdk import (
     Stack,
 )
 from constructs import Construct
-LAMBDA_DIR = "src/lambda"
+
+from backend.our_function import OurFunction
+
+LAMBDA_DEP = "lambda/dependencies"
+LAMBDA_SRC = "lambda/src"
 
 class BackendStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        _lambda.Function(
+        self.dependency_layer = _lambda.LayerVersion(
+            self,
+            "DependencyLayer",
+            code=_lambda.Code.from_asset(LAMBDA_DEP),
+            compatible_runtimes=[_lambda.Runtime.PYTHON_3_12],
+            description="A layer with shared dependencies between lambdas",
+        )
+
+
+        hello_lambda = OurFunction(
             self,
             "HelloLambda",
             runtime=_lambda.Runtime.PYTHON_3_12,
--- backend/lambda/src/hello/hello.py
+++ backend/lambda/src/hello/hello.py
@@ -1,4 +1,9 @@
+import PIL.Image
+
+
 def handler(event, _):
     greeting = f"Hello {event['name']}!"
+    new_image = PIL.Image.new("RGB", (60, 30), color = "red")
     print(greeting)
+    print(new_image.size)
     return {"greeting" : greeting}
