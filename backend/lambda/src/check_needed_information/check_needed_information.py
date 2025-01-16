c2c918ccb9d24b3eb81d11a8a2619b50c7e796c9
anna-pralat-wttech <anna.pralat@wundermanthompson.com>
2025-01-16 20:16:47 +0100

    prompt

--- backend/lambda/src/check_needed_information/check_needed_information.py
+++ backend/lambda/src/check_needed_information/check_needed_information.py
@@ -11,14 +11,27 @@ table = dynamodb.Table(os.environ['OFFER_CREATORS_TABLE'])
 prompt = """
 
 Human:
-Please review the attached database row to ensure all fields are complete. 
+Please review the attached database row to ensure all fields are complete.
+Response only in JSON format with the following fields:
+- all_data_collected: (true if needed information is complete, false if not)
+- answer: (a concise message for the user)
 Identify any null or missing fields, and prioritize them by importance if multiple fields are incomplete. 
 
-Based on your analysis, generate a very short, one sentence, concise and friendly message for the user. 
-The message should indicate which part of the book they should photograph to provide the missing information.
+Based on your analysis, generate a very short, one sentence, concise, precise and friendly message for the user. 
+The message should indicate which part of the book user should photograph to provide the missing information.
+If all fields are complete, please indicate that all data has been collected.
+Please ensure the response is in valid JSON format.
 
 Assistant:"""
-
+# prompt = """
+#
+# Human:
+# Please analyze this image and provide information in JSON format with the following fields:
+# - is_book: (true/false)
+# Please ensure the response is in valid JSON format.
+# Feel free to include any relevant details or observations in the answer field.
+#
+# Assistant:"""
 def lambda_handler(event, context):
     body = json.loads(event['body'])
     user_id = body.get("user_id")
