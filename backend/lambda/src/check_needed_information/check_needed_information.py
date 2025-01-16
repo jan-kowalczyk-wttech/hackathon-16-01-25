e308b9826777eafa4f2d95c44827cf0b86f2bb61
jan.kowalczyk <jan.kowalczyk@vml.com>
2025-01-16 20:16:24 +0100

    working

--- backend/lambda/src/check_needed_information/check_needed_information.py
+++ backend/lambda/src/check_needed_information/check_needed_information.py
@@ -1,3 +1,6 @@
+import uuid
+from typing import Any
+
 import boto3
 import os
 import json
@@ -6,7 +9,7 @@ region = "us-west-2"
 runtime = boto3.client("bedrock-runtime", region)
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table(os.environ['OFFER_CREATORS_TABLE'])
+table = dynamodb.Table(os.environ['ACTIONS_TABLE'])
 
 prompt = """
 
@@ -19,25 +22,35 @@ The message should indicate which part of the book they should photograph to pro
 
 Assistant:"""
 
+def get_empty_items(creator_results):
+    all_items = {}
+    for creator_result in creator_results:
+        data = json.loads(creator_result["result"])
+        print(data)
+        for key, value in data.items():
+            if all_items.get(key) is None:
+                all_items.update({key: value})
+
+    null_items = {}
+    for key, value in all_items.items():
+        if value is None:
+            null_items.update({key: value})
+
+    return null_items
+
 def lambda_handler(event, context):
     body = json.loads(event['body'])
-    user_id = body.get("user_id")
-    creator_id = body.get("creator_id")
-    
-    try:
-        response = table.get_item(Key={'id': creator_id})
-        item = response.get('Item')
-        if not item:
-            return {
-                'statusCode': 404,
-                'body': json.dumps({'error': 'Offer not found'})
-            }
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'error': str(e)})
-        }
-    
+    user_id = body['user_id']
+    creator_id = body['creator_id']
+
+    responses = table.scan().get('Items')
+    creator_results = []
+    for response in responses:
+        if response['creator_id'] == creator_id and response["action_name"] == "define_object":
+            creator_results.append(response)
+
+    empty_items = get_empty_items(creator_results)
+
     body = json.dumps(
         {
             "anthropic_version": "bedrock-2023-05-31",
@@ -46,7 +59,7 @@ def lambda_handler(event, context):
             "messages": [
                 {
                     "role": "user",
-                    "content": json.dumps(item)
+                    "content": json.dumps(empty_items)
                 }
             ],
         }
@@ -56,11 +69,25 @@ def lambda_handler(event, context):
         modelId="anthropic.claude-3-sonnet-20240229-v1:0",
         body=body
     )
+
     response_body = json.loads(response.get("body").read())
+
+    result = response_body['content'][0]['text']
+    item = get_action_item(user_id, creator_id, result)
+    dynamodb.Table(table).put_item(Item=item)
     
     return {
         'statusCode': 200,
-        'body': json.dumps({'message': response_body['content'][0]['text']})
+        'body': json.dumps({'answer': result})
     }
-    
-    
+
+def get_action_item(user_id: str, creator_id: str, result: Any):
+    id = str(uuid.uuid4())
+    action_name = "check_needed_information"
+    return {
+        "id": id,
+        "user_id": user_id,
+        "creator_id": creator_id,
+        "action_name": action_name,
+        "result": result
+    }
